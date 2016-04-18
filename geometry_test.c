de5d6ae7b50ea458d2d15656b96f837ca60cd81a
SlayerWatcher6 <obibrian@gmail.com>
2016-04-18 13:40:13 -0600

    Update geometry_test.c

--- geometry_test.c
+++ geometry_test.c
@@ -145,6 +145,22 @@ START_TEST(test_2d_midpoint)
 }
 END_TEST
 
+START_TEST(test_area)
+{
+    coord_2d_t a;
+    coord_2d_t b;
+    coord_2d_t c;
+    
+    a.x = 5;
+    a.y = 10;
+    b.x = 7;
+    b.y = 9;
+    c.x = 13;
+    c.y = 3;
+    ck_assert(coord_2d_area_triangle(&a, &b, &c))
+}
+END_TEST
+
 /* coord_2d Test Suite */
 Suite* coord_2d_suite(void)
 {
@@ -161,11 +177,15 @@ Suite* coord_2d_suite(void)
 
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
+    
+    TCase* tc_2d_area = tcase_create("coord_2d_area_triangle");
+    tcase_add_test(tc_2d_area, test_area);
 
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
     suite_add_tcase(s, tc_2d_dist);
     suite_add_tcase(s, tc_2d_midpoint);
+    suite_add_tcase(s, tc_2d_area);
 
     /* Return Suite */
     return s;
